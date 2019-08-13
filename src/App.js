e9445bd84e93d0be6e873012778d1664c0b63715
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-13 11:13:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculator extends React.Component {
+  state = {
+    count: [],
+    keys: [
+      { input: "clear", buttonType: "clearBtn" },
+      { input: "/", buttonType: "operatorBtn" },
+      { input: "7", buttonType: "numberBtn" },
+      { input: "8", buttonType: "numberBtn" },
+      { input: "9", buttonType: "numberBtn" },
+      { input: "*", buttonType: "operatorBtn" },
+      { input: "4", buttonType: "numberBtn" },
+      { input: "5", buttonType: "numberBtn" },
+      { input: "6", buttonType: "numberBtn" },
+      { input: "-", buttonType: "operatorBtn" },
+      { input: "1", buttonType: "numberBtn" },
+      { input: "2", buttonType: "numberBtn" },
+      { input: "3", buttonType: "numberBtn" },
+      { input: "+", buttonType: "operatorBtn" },
+      { input: "0", buttonType: "numberBtn" },
+      { input: "=", buttonType: "eqBtn" }
+    ]
+  }
+  number = calcInput => {
+    if (calcInput === "clear") {
+      this.setState({ count: [] })
+    } else if (calcInput === "=") {
+      this.setState(prevState => ({
+        count: [eval(prevState.count.join(""))]
+      }))
+    } else {
+      this.setState(prevState => ({
+        count: [...prevState.count, calcInput]
+      }))
+    }
+  }
+  render() {
+    return (
+      <div className="calc">
+        <div className="row">
+          <p> {this.state.count} </p>
+        </div>
+        <div className="buttonsWrapper">
+          <div className="buttons">
+            {this.state.keys.map(key => {
+              return (
+                <button
+                  className={key.buttonType}
+                  onClick={() => this.number(key.input)}
+                >
+                  {key.input}
+                </button>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default Calculator; 
